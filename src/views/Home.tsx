7150dbc733a3639fbcdcaef701a658823ea3cc5c
Milito de Skorpio <arcadio.2es0@gmail.com>
2024-06-12 05:05:16 -0600

    HomeHTML

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -2,6 +2,39 @@
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <>
+    <div>
+     <h1></h1>
+     <h2></h2>
+     <button></button> 
+    </div>
+    {/* quienes somos */}
+    <div>
+      <h1></h1>
+      <p></p>
+      <button></button>
+    </div>
+    {/* proyectos */}
+    <div>
+      <h1></h1>
+      <p></p>
+      <button></button>
+    </div>
+    {/* Area de Actuacion */}
+    <div>
+      {/* Card */}
+      <div>
+        <img src="" alt="" />
+        <h3></h3>
+        <p></p>
+      </div>
+    </div>
+    <div>
+      <h2></h2>
+      <p></p>
+    </div>
+    </>
+    
   )
 }
+
