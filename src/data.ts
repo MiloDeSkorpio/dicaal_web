06a83d11f5e9f359cbd0b53e5051515c326aeff1
Milito de Skorpio <arcadio.2es0@gmail.com>
2024-06-12 05:02:41 -0600

    DataFeaturesText

--- src/data.ts
+++ src/data.ts
@@ -23,122 +23,141 @@ export const nav = [
   
 ];
 
-// export const hero = {
-//   title: '¿Quienes Somos?',
-//   subtitle: 'Somos una firma que se está abriendo camino a nivel nacional e internacional y se esta posicionando como una de las mejores brindando un servicio profesional y de vanguardia',
-//   btnText: 'Ver más',
-//   compText: '— Misión, Visión y  Catalogo de Servicios',
-//   image: HeroImg,
-// };
 
-// export const features = {
-//   feature1: {
-//     pretitle: 'Las Mejores Soluciones',
-//     title: 'Misión',
-//     subtitle:
-//       'Tener las mejores soluciones de nuestros clientes completamente satisfechos, dando prioridad a la confianza mutua, para que su situacion financiera este transparente, para el beneficio del usuario',
-//     btnLink: 'Visión',
-//     btnIcon: ArrowRightImg,
-//     image: Feature1Img,
-//   },
-//   feature2: {
-//     pretitle: 'Confiabilidad',
-//     title: 'Vision',
-//     subtitle:
-//       'Liderar como una de las firmas más confiables a nivel nacional e internacional, llevando de la mano a nuestros clientes con nuestros servicios, que lleguen al nivel que desean',
-//     btnLink: 'Servicios',
-//     btnIcon: ArrowRightImg,
-//     image: Feature2Img,
-//   },
-//   feature3: {
-//     pretitle: 'Servicios',
-//     title: 'Catalogo de Servicios',
-//     subtitle:
-//       'Contamos con un amplio catalogo de servicios',
-//     btnLink1: 'Contable y Fiscal',
-//     btnLink2: 'Laboral y Administrativo',
-//     btnIcon: ArrowRightImg,
-//     image: Feature3Img,
-//   },
-// };
+export const homeFeatures = {
+  feature1: {
+    title: 'Ilumina tus interiores',
+    subtitle:
+      'Arquitectura de Aluminio',
+    btnLink: 'Contactenos',
+    // image: Feature1Img,
+  },
+  feature2: {
+    title: '¿Quienes somos?',
+    textContent:
+      'Somos una empresa especialista con más de 10 años de experiencia ofreciendo soluciones arquitectónicas de aluminio y vidfrio para todo tipo de proyectos hecho a la medida de sus necesidades. Nuestro portafolio ofrece una amplia gama de servicios en ventanas de lineas: nacionales, europea y española.',
+    textContent2: 'Desarrollamos trabajos en vidrio templado vidrio laminado (doble) y peliculas de seguridad, ademas de contar con canceleria para oficinas y accesos, canceleria comercial, canceleria para baños, techos, domos, barandales y pergolas. Contamos con el quipo para trabajar en obras de alto nivel.',
+    btnLink: 'Conoce más de nosotros',
+    // image: Feature2Img,
+  },
+  feature3: {
+    title: 'Proyectos Personalizados',
+    textContent:
+      'En DICAAL diseñamos proyectos personalizados en aluminio y vidrio que no solo son funcionales, si no tambien esteticamente atractivas y duraderas para tu espacio.',
+    btnLink1: 'Conoce más de nuestros proyectos',
 
-// export const product = {
-//   title: 'Contable y Fiscal',
-//   img: ContableImg,
-//   cards: [
-//     {
-//       title: 'Cumplimiento de obligaciones fiscales',
-//       delay: 100
-//     },
-//     {
-//       title: 'Regularización de ejercicios de años anteriores',
-//       delay: 200
-//     },
-//     {
-//       title: 'Facturación Electronica',
-//       delay: 300
-//     },
-//     {
-//       title: 'Emisión de Estados Financieros',
-//       delay: 400
-//     },
-//     {
-//       title: 'Diagnósticos Fiscales',
-//       delay: 500
-//     },
-//     {
-//       title: 'Control de Presupuestos',
-//       delay: 500
-//     },
-//     {
-//       title: 'Inventarios',
-//       delay: 400
-//     },
-//     {
-//       title: 'Devoluciones y Seguimiento',
-//       delay: 300
-//     },
-//     {
-//       title: 'Estrategias Fiscales',
-//       delay: 200
-//     },
-//     {
-//       title: 'Análisis de Riesgos',
-//       delay: 100
-//     },
-//   ],
-// };
+    // image: Feature3Img,
+  },
+  feature4: {
+    // Despues de Area de Actuacion
+    title: 'Aseguramos el éxito de tus proyectos.',
+    textContent:
+      'Confie en DICAAL para garantizar el éxito y la excelencia en cada obra. Nuestra dedicacion y compromiso nos hacen la eleccion ideal para construtoras que buscan lo mejor en Aluminio y Vidrio.',
+    textContent2: 'Contáctenos hoy mismo y descubra por que somos su socio estrategico.',
 
-// export const pricing = {
-//   title: 'Laboral y Administrativo',
-//   img: LaboralImg,
-//   cards: [
-//     {
-//       title: 'Formalización de Nominas',
-//       delay: 100
-//     },
-//     {
-//       title: 'Altas Bajas IMSS',
-//       delay: 200
-//     },
-//     {
-//       title: 'Esquemas de remuneraciones',
-//       delay: 300
-//     },
-//     {
-//       title: 'Formalización de Declaraciones IMSS,ICSOE, y SISUB',
-//       delay: 400
-//     },
-//     {
-//       title: 'Formalización de contratos laborales y sus prórogas',
-//       delay: 500
-//     },
-//     {
-//       title: 'Tramite y seguimiento para REPSE',
-//       delay: 700
-//     }
-//   ],
-// };
+    // image: Feature3Img,
+  },
+  feature5: {
+    // image: Feature3Img, -- logo de paloma en circulo
+    title: 'Calidad en cada detalle.',
+    textContent:
+      'Nuestro compromiso con la excelencia se refleja en la meticulosa seleccion de materiales en aluminio y vidrio de primera calidad, para asegurar la durabilidad, resistencia y estetica de cada pieza que creamos.',
+  },
+  feature6: {
+    // image: Feature3Img, -- logo de diamante
+    title: 'Estilo en cada rincón',
+    textContent:
+      'Nuestros barandales son la elección perfecta para darle un toque sofisticado a cada espacio. Esta imagen es la prueba de ello: un ejemplo inspirador de cómo nuestros diseños resaltan la belleza y la transparecnia en cualquier entorno',
+  },
+  feature7: {
+    // image: Feature3Img, -- logo de usuario en rounde cube
+    title: 'Satisfaccion de Clientes',
+    textContent:
+      'La satisfaccion de nuestros clientes es la maxima prioridad es por eso que nos esforzamos en ofecerle soluciones de aluminio y vidrio de la más alta calidad, respaldadas por un serrvicio excepcional.',
+  },
+  feature8: {
+    // image: Feature3Img, -- logo de paloma en rounde cube
+    title: 'Garantia',
+    textContent:
+      'Respaldamos ada uno de nuestros servicios con nuestra solida Garantia de satisfaccion total. Estamos seguros de  la calidad de nuestro trabajo lo que te ofrece una tranquilidad y confianza en cada proyecto',
+  },
+  feature9: {
+    // image: Feature3Img, -- logo de paloma en rounde cube
+    title: 'Tiempo de respuesta',
+    textContent:
+      'En DICAAL nos comprometemos a ofreceerte una respuesta rapida y soluciones inmediatas a tus necesidades.',
+  },
+};
+
+export const herrajes = {
+  feature1: {
+    title: 'En DICAAL ofrecemos una gran variedad de herrajes para la venta al publico.',
+    text: 'En nuestra coleccion de herrajes para aluminio y vidrio, encontraras soluciones excepcionales para tus proyectos, desde bisagras y soportes hasta sistemas de sujecion y cerraduras, cada uno de nuestros productos esta diseñado para ofrecer una combinacion perfecta de rendimiento confiable y elegancia moderna.',
+    // image: Feature1Img,
+  },
+  feature2: {
+    title: 'Herrajes de Aluminio',
+    text:
+      'Ofrecemos una amplia variedad de herrajes para todo tipo de proyectos de aluminio. Nuestros productos son la eleccion perfecta para tus necesidades. Contactanos hoy para llevar tus proyectos al siguiente nivel.',
+    btnLink: 'Ver Catálogo',
+    // image: Feature1Img,
+  },
+  feature3: {
+    title: 'Herrajes para vidrio',
+    text:
+      'Con un enfoque en la estetica, nuestris herrajespara vidrio realzan la belleza aportando un toque de estilo y sofisticación a cualquier proyecto.',
+    btnLink: 'Ver Catálogo',
+    // image: Feature1Img,
+  },
+}
+
+export const homeCards = {
+  title: 'Área de Actuación',
+  // img: ContableImg,
+  cards: [
+    {
+      // img: img,
+      title: 'Comercios',
+      text: 'El aluminio y el vidrio son materiales que aportan una apariencia sofisticada y elegante a tu negocio. Su combinacion puede crear una imagen moderna y atractiva que atraiga a los clientes y refleja la vision de tu marca.'
+    },
+    {
+      // img: img,
+      title: 'Oficinas',
+      text: 'Transformamos tu espacio de trabajo con la combinacion perfecta de elegancia y modernidad, da un toque de distincion con nuestros canceles de aluminio y vidrio.'
+    },
+    {
+      // img: img,
+      title: 'Hogar',
+      text: 'Nos especialozamos en crear un espacio innovador y acogedor para tu hogar desde ventaans hasta domos para brindar una mayor luminocidad en cada rincon.'
+    },
+
+
+  ],
+};
+
+export const nostros = {
+  title: 'Voz de nuestra Marca',
+  // img: LaboralImg,
+  cards: [
+    {
+      // img: ImGift,
+      title: 'Misión',
+      p: 'Diseñar, fabricar y distribuir productos de cidrio y aluminio de vanguardia comprometidos con la sotenibilidad, la eficiencia y la satisfaccion del cliente impulsamos el desarrollo de nuestros colaboradores y contribuimos al progreso de las comunidades donde operamos promoviendo un entorno de trabajo seguro y etico.'
+    },
+    {
+      // img: ImGift,
+      title: 'Visión',
+      p: 'Ser lideres en innovacion, tecnologica y excelencia en la industria del vidrio y aluminio, proporcionando soluciones de calidad que transformen los espacios y mejoren la vida de las personas.'
+    },
+    {
+      // img: ImGift,
+      title: 'Valores',
+      p: 'En DICAAL la calidad, la innovacion y el compromiso con la satisfaccion del cliente son nuestros valores fundamentales.',
+      p2: 'Descubre como podemos hacer realidad tus proyectos con excelencia y profesionalismo.'
+    },
+    
+  ],
+};
 
 // export const footer = {
 //   logo: LogoImg,
