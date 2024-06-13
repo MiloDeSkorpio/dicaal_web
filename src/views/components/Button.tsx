c589df2583942ee66e117391df33b63cf5b15d08
Milito de Skorpio <arcadio.2es0@gmail.com>
2024-06-13 09:24:11 -0600

    HoverBtn

--- src/views/components/Button.tsx
+++ src/views/components/Button.tsx
@@ -5,7 +5,7 @@ export default function Button(params: {name: string; href: string; }) {
     <div>
       <a 
         href={params.href} 
-        className="px-6 py-3 bg-[#030b31] text-white rounded-md"
+        className="px-6 py-3 bg-[#131b46] hover:bg-[#030b31] text-white rounded-md"
         >{params.name}</a>
     </div>
   )
