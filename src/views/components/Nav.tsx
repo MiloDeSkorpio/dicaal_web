8a1c5f765aafa460644437dfbd0b2c30f8c26f90
Milito de Skorpio <arcadio.2es0@gmail.com>
2024-06-13 09:11:28 -0600

    Hover style

--- src/views/components/Nav.tsx
+++ src/views/components/Nav.tsx
@@ -8,8 +8,8 @@ export default function Nav() {
         //destructure item
         const { href, name } = item
         return (
-          <li key={index} className="">
-            <a className='text-xl font-bold text-slate-500 hover:text-slate-600 ' href={href}>
+          <li key={index} className="hover:border-b-2 hover:shadow hover:border-gray-900">
+            <a className='text-xl font-semibold text-gray-700 hover:text-gray-900 ' href={href}>
               {name}
             </a>
           </li>
