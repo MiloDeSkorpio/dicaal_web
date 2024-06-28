78c541711815bc1d9247636c7af19d5dd782eb5b
Milito de Skorpio <arcadio.2es0@gmail.com>
2024-06-28 10:54:40 -0600

    Feature5_Responsive

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -8,17 +8,19 @@ export default function Home() {
   const { cards } = homeCards
   return (
     <div>
-      <div className="feature1 ">
-        <div className="m-auto w-full bg-[rgba(0,0,0,0.55)] space-y-8 min-h-[770px] flex flex-col items-center text-center justify-center">
-          <h1 className="text-white text-6xl font-black shadow-gray-950" >{feature1.title}</h1>
-          <h2 className="text-white text-4xl font-bold shadow-gray-950">{feature1.subtitle}</h2>
+      <section className="feature1 h-screen">
+        <div className="bg-[rgba(0,0,0,0.55)] min-h-screen w-full m-auto space-y-[90px] flex flex-col items-center justify-center">
+          <div className=" space-y-5 text-center">
+            <h1 className="text-white text-6xl font-black shadow-gray-950" >{feature1.title}</h1>
+            <h2 className="text-white text-4xl shadow-gray-950">{feature1.subtitle}</h2>
+          </div>
           <Button href={feature1.btnLink} name={feature1.btnName} />
         </div>
-      </div>
+      </section>
       {/* quienes somos */}
       <section className="container m-auto max-w-6xl space-y-8 text-center">
         <h1 className="mt-5 text-6xl text-[#0d497f]">{feature2.title}</h1>
-        <p className="leading-loose p-4 text-base lg:text-xl">{feature2.textContent} {feature2.textContent2}</p>
+        <p className="leading-loose lg:leading-10 p-4 text-base lg:text-xl ">{feature2.textContent} {feature2.textContent2}</p>
         <Button href={feature2.btnLink} name={feature2.btnName} />
       </section>
       {/* proyectos */}
@@ -37,7 +39,7 @@ export default function Home() {
           <hr className="border-[1px] border-gray-400" />
         </div>
         {/* Card */}
-        <div className="lg:flex w-auto">
+        <div className="lg:flex w-auto bg-gray-300 py-2">
           <Card
             src={cards[0].img}
             alt={cards[0].alt}
@@ -68,29 +70,29 @@ export default function Home() {
       />
       {/*  */}
       <section className="mt-10">
-        <div  className="lg:flex">
-          <div className="h-24 lg:h-auto lg:w-1/2 bg-cover overflow-hidden">
+        <div  className="lg:flex lg:max-h-[500px]">
+          <div className="h-24 md:h-36 lg:h-auto  lg:w-1/2 bg-cover overflow-hidden ">
             <img src={feature5.image} alt={feature5.alt} />
           </div>
-          <div className="flex flex-col p-5 text-center items-center lg:w-1/2">
-            <img src={feature5.icon} alt={feature5.altIco} className="w-12 h-12" />
-            <h2 className="text-2xl my-5 font-semibold">{feature5.title}</h2>
+          <div className="flex flex-col p-16 text-center items-center lg:w-1/2 justify-center">
+            <img src={feature5.icon} alt={feature5.altIco} className="w-20 h-20" />
+            <h2 className="text-4xl my-5 font-semibold">{feature5.title}</h2>
             <p>{feature5.textContent}</p>
           </div>
         </div>
-        <div className="lg:flex">
-          <div className="flex flex-col p-5 text-center items-center lg:w-1/2">
-            <img src={feature6.icon} alt={feature6.altIco} className="w-12 h-12" />
-            <h2 className="text-2xl my-5 font-semibold">{feature6.title}</h2>
+        <div className="lg:flex lg:max-h-[500px]">
+          <div className="flex flex-col p-16 text-center items-center lg:w-1/2 justify-center">
+            <img src={feature6.icon} alt={feature6.altIco} className="w-20 h-20" />
+            <h2 className="text-4xl my-5 font-semibold">{feature6.title}</h2>
             <p>{feature6.textContent}</p>
           </div>
-          <div className="h-24 lg:h-auto lg:w-1/2 bg-cover overflow-hidden">
+          <div className="h-24 md:h-36 lg:h-auto lg:w-1/2 bg-cover overflow-hidden ">
             <img src={feature6.image} alt="" />
           </div>
         </div>
       </section>
       {/*  */}
-      <section>
+      <section className="mt-28 lg:flex lg:justify-between">
         <div className=" flex flex-col items-center p-5 text-center">
           <div className="bg-[#030b31] rounded-md h-20 w-20 flex items-center justify-center">
             <img src={feature7.icon} alt="" />
