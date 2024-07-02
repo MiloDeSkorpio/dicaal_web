7699dd79e9b791756897dd83e9ceb9519225b398
Milito de Skorpio <arcadio.2es0@gmail.com>
2024-07-02 09:27:04 -0600

    NoButtonHerrajes

--- src/views/Nosotros.tsx
+++ src/views/Nosotros.tsx
@@ -1,7 +1,19 @@
+import { nostros } from "../data"
+import CardUs from "./components/CardUs"
+import Hero from "./components/Hero"
 
 
 export default function Nosotros() {
+  const { title, cont, cards } = nostros
   return (
-    <div>Nosotros</div>
+    <main className="space-y-5">
+      <Hero cont={cont} title={title} />
+      <section className="bg-gray-300 w-full py-10" >
+        {cards.map((card, index) => (
+          <CardUs key={index} src={card.img} alt={card.alt} title={card.title} text={card.p} />
+        ))}
+
+      </section>
+    </main>
   )
 }
