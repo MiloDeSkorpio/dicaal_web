f66ad7a6c2217d247bd1f2786619fac29f734f5e
Milito de Skorpio <arcadio.2es0@gmail.com>
2024-07-02 20:54:30 -0600

    Revert "NoButtonHerrajes"
    This reverts commit 7699dd79e9b791756897dd83e9ceb9519225b398.

--- src/views/Nosotros.tsx
+++ src/views/Nosotros.tsx
@@ -1,19 +1,7 @@
-import { nostros } from "../data"
-import CardUs from "./components/CardUs"
-import Hero from "./components/Hero"
 
 
 export default function Nosotros() {
-  const { title, cont, cards } = nostros
   return (
-    <main className="space-y-5">
-      <Hero cont={cont} title={title} />
-      <section className="bg-gray-300 w-full py-10" >
-        {cards.map((card, index) => (
-          <CardUs key={index} src={card.img} alt={card.alt} title={card.title} text={card.p} />
-        ))}
-
-      </section>
-    </main>
+    <div>Nosotros</div>
   )
 }
