8448e4943320b8ba5c4dd3a1b206ec834d6e5671
Milito de Skorpio <arcadio.2es0@gmail.com>
2024-07-02 21:02:16 -0600

    AdjsutTextCenterHeroComp

--- src/views/components/Hero.tsx
+++ src/views/components/Hero.tsx
@@ -3,7 +3,7 @@
 export default function Hero(params: { cont: any; title: any }) {
   return (
     <section className={params.cont}>
-      <div className="w-full h-full bg-[rgba(0,0,0,0.55)] flex items-center text-center">
+      <div className="w-full h-full bg-[rgba(0,0,0,0.55)] flex items-center text-center justify-center">
         <h1 className="text-white text-2xl md:text-4xl lg:text-6xl font-black shadow-gray-950 ">{params.title}</h1>
       </div>
     </section>
