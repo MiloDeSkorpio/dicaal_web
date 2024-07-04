b6e6e22b67cbebd8e0249b434abecfa96795b621
Milito de Skorpio <arcadio.2es0@gmail.com>
2024-07-04 08:08:46 -0600

    StrcutureContact&portfolio

--- src/views/Contacto.tsx
+++ src/views/Contacto.tsx
@@ -1,7 +1,34 @@
 
 
 export default function Contacto() {
-  return (
-    <div>Contacto</div>
+  return (  
+    <section>
+      {/* <form action="">
+        <div>
+          <label htmlFor="" />
+          <input type="text" name="" id="" />
+        </div>
+        <div>
+          <label htmlFor="" />
+          <input type="number" name="" id="" />
+        </div>
+        <div>
+          <label htmlFor="" />
+          <input type="email" name="" id="" />
+        </div>
+        <div>
+          <label htmlFor="" />
+          <input type="text" name="" id="" />
+        </div>
+        <div>
+          <label htmlFor="" />
+          <input type="text" name="" id="" />
+        </div>
+        <div>
+          <label htmlFor="" />
+          <textarea name="" id="" />
+        </div>
+      </form> */}
+    </section>
   )
 }
--- src/views/Portafolio.tsx
+++ src/views/Portafolio.tsx
@@ -2,6 +2,15 @@
 
 export default function Portafolio() {
   return (
-    <div>Portafolio</div>
+    <main>
+      <section>
+        <h1></h1>
+        <h2></h2>
+        <p></p>
+        <div>
+          
+        </div>
+      </section>
+    </main>
   )
 }
